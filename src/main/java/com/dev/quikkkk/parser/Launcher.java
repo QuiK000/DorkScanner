235896ed1aa6b8269a7da0332c6c660ff799f8b2
QuiK000 <teamquiksteam@gmail.com>
2026-02-19 14:59:43 +0200

    Fix Launcher to properly start the App
    - Update Launcher to launch App.class instead of HelloApplication.class
    - Ensure correct application entry point for JavaFX

--- src/main/java/com/dev/quikkkk/parser/Launcher.java
+++ src/main/java/com/dev/quikkkk/parser/Launcher.java
@@ -1,9 +1,10 @@
 package com.dev.quikkkk.parser;
 
+import com.dev.quikkkk.parser.app.App;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(App.class, args);
     }
 }
