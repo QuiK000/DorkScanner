d1ad8604f6e9e688f755bd8370fe79891d7634ac
QuiK000 <teamquiksteam@gmail.com>
2026-02-19 17:00:56 +0200

    Add manual captcha handling to GoogleSearchEngine

--- src/main/java/com/dev/quikkkk/parser/infrastructure/search/GoogleSearchEngine.java
+++ src/main/java/com/dev/quikkkk/parser/infrastructure/search/GoogleSearchEngine.java
@@ -12,11 +12,13 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @AllArgsConstructor
 public class GoogleSearchEngine implements ISearchEngine {
     private final ProxyManager proxyManager;
+    private final boolean manualCaptcha;
 
     @Override
     public String getName() {
@@ -25,12 +27,13 @@ public class GoogleSearchEngine implements ISearchEngine {
 
     @Override
     public Set<SearchResult> search(String dork, int limit) throws InterruptedException {
-        WebDriver driver = proxyManager.createDriver();
+        WebDriver driver = proxyManager.createDriver(manualCaptcha);
         Set<SearchResult> results = new HashSet<>();
 
         try {
             driver.get("https://www.google.com/search?q=" + URLEncoder.encode(dork, StandardCharsets.UTF_8));
             while (results.size() < limit) {
+                waitForCaptcha(driver);
                 Thread.sleep(2500);
                 List<WebElement> links = driver.findElements(By.cssSelector("div.yuRUbf a"));
                 for (WebElement el : links) {
@@ -47,5 +50,14 @@ public class GoogleSearchEngine implements ISearchEngine {
         }
 
         return results;
+    }
+
+    private void waitForCaptcha(WebDriver driver) throws InterruptedException {
+        if (manualCaptcha) {
+            while (Objects.requireNonNull(driver.getCurrentUrl()).contains("sorry/index")) {
+                System.out.println("Waiting to resolve captcha");
+                Thread.sleep(3000);
+            }
+        }
     }
 }
