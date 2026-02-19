90144f161b1e9d304ed6be1fff2c1e7d700742ab
QuiK000 <teamquiksteam@gmail.com>
2026-02-19 16:36:11 +0200

    Removed module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.dev.quikkkk.parser {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.dev.quikkkk.parser to javafx.fxml;
-    exports com.dev.quikkkk.parser.app;
-}
